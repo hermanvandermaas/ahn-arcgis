293143d7e4d1febb9c105ae60747909ca13b7ce0
Herman van der Maas <herman@waywayway.nl>
2018-01-08 06:58:30 +0100

    Werkende Google map

--- app/src/main/java/nl/waywayway/ahn/MainActivity.java
+++ app/src/main/java/nl/waywayway/ahn/MainActivity.java
@@ -1,14 +1,29 @@
 package nl.waywayway.ahn;
 
-import android.app.*;
 import android.os.*;
+import android.support.v7.app.*;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
 
-public class MainActivity extends Activity 
+public class MainActivity extends AppCompatActivity
+implements OnMapReadyCallback
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		MapFragment mapFragment = (MapFragment) getFragmentManager()
+            .findFragmentById(R.id.map);
+		mapFragment.getMapAsync(this);
+    }
+	
+	public void onMapReady(GoogleMap googleMap)
+	{
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+							.title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
